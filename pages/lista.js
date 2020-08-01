dbe2d78cbd260bd6c155946b65c6dccc98427881
Tomás <tomygmbb@gmail.com>
2020-07-31 23:23:58 -0400

    Prototipo Mockup #5 / NavBar
    Redistribución de selectores + prototipo del NavBar(se creó como elemento)

--- pages/lista.js
+++ pages/lista.js
@@ -2,6 +2,7 @@
 import dataMalla from "../data.json";
 import { css } from "@emotion/react";
 import { useState } from "react";
+import navBar from "./navbar";
 
 const semestres = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 
@@ -10,60 +11,91 @@ const PaginadeLista = () => {
   const [semestreActive, setSemestreActive] = useState(null);
 
   return (
-    <div>
-      {semestres.map((semestre) => {
-        return (
-          <button
-            key={semestre}
-            onClick={() => {
-              setSemestreActive(semestre);
-            }}
-          >
-            Semestre {semestre}
-          </button>
-        );
-      })}
-      <br />
+    <div
+      css={css`
+        width: 100%;
+        display: block;
+        border: 1px solid red;
+      `}
+    >
+      <nav>{navBar()}</nav>
       <div
         css={css`
+          border: 1px solid green;
+          float: left;
           display: flex;
-          flex-direction: column;
+          width: 100%;
+          flex-direction: row;
         `}
       >
-        {dataMalla.malla.map((curso) => {
-          return semestreActive === curso.semester ? (
-            <button
-              css={css`
-                background-color: ${cursosClickeados[curso.id]
-                  ? "black"
-                  : "white"};
-                border-radius: 15px;
-                padding: 10px;
-                font-size: 1.3em;
-                font-weight: bold;
-                background-color: #79c4e3;
-                color: white;
-                transition: box-shadow 0.5s;
-                color: ${cursosClickeados[curso.id] ? "white" : "black"};
-              `}
-              key={curso.id}
-              onClick={() => {
-                setCursosClickeados(() => {
-                  return {
-                    ...cursosClickeados,
-                    [curso.id]: !(
-                      cursosClickeados[
-                        curso.id
-                      ] /** indefinido o true o false */
-                    ),
-                  };
-                });
-              }}
-            >
-              {curso.name}
-            </button>
-          ) : null;
-        })}
+        <div
+          css={css`
+            border: 1px solid black;
+            display: flex;
+            flex-direction: column;
+            width: 50%;
+            float: left;
+          `}
+        >
+          {semestres.map((semestre) => {
+            return (
+              <button
+                css={css`
+                  font-size: 16px;
+                `}
+                key={semestre}
+                onClick={() => {
+                  setSemestreActive(semestre);
+                }}
+              >
+                Semestre {semestre}
+              </button>
+            );
+          })}
+        </div>
+        <div
+          css={css`
+            display: flex;
+            float: right;
+            border: 1px solid black;
+            flex-direction: column;
+            width: 50%;
+          `}
+        >
+          {dataMalla.malla.map((curso) => {
+            return semestreActive === curso.semester ? (
+              <button
+                css={css`
+                  background-color: ${cursosClickeados[curso.id]
+                    ? "#fa9703"
+                    : "#9511ec"};
+                  border-radius: 0px;
+                  padding: 10px 10px;
+                  font-size: 12px;
+                  font-weight: bold;
+
+                  color: white;
+                  transition: box-shadow 0.5s;
+                `}
+                key={curso.id}
+                onClick={() => {
+                  setCursosClickeados(() => {
+                    return {
+                      ...cursosClickeados,
+                      [curso.id]: !(
+                        cursosClickeados[
+                          curso.id
+                        ] /** indefinido o true o false */
+                      ),
+                    };
+                  });
+                }}
+              >
+                {curso.name}
+              </button>
+            ) : null;
+          })}
+        </div>
       </div>
     </div>
   );
