21a89b5008668c5a5913b439a95b9f7179d4406f
Nicolas Saldivia <k0k05sal12@gmail.com>
2020-08-07 19:49:07 -0400

    Avance algoritmo generar

--- src/api/algoritmo1.js
+++ src/api/algoritmo1.js
@@ -1,5 +1,29 @@
 import data from "../../data.json";
 
+var hash = {};
+data.malla.map((element) => {
+  hash[element.id] = element;
+});
+
 export const algoritmo = (cursosAprobados, cursosPendientes) => {
-  return [];
+  const creditsProm = 31;
+  const plan = [];
+  const aprobadosN = cursosAprobados.map((n) => {
+    return hash[n].course_id;
+  });
+  const potenciales = cursosPendientes.map((curso) => {
+    if (curso.requisites === "") {
+      return curso.id;
+    } else if (
+      curso.requisites.split(",").filter((elemento) => {
+        return !aprobadosN.includes(elemento);
+      }).length == 0
+    ) {
+      return curso.id;
+    } else {
+      return "";
+    }
+  });
+
+  return potenciales;
 };
